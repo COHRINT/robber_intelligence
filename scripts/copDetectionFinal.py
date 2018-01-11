9c3711936d6c7a087800a115597665959978e3e8
sousheel <sousheel.vunnam@colorado.edu>
2018-01-11 09:38:47 -0700

    merged cop_evasion and goal_oriented_nav

--- scripts/copDetectionFinal.py
+++ scripts/copDetectionFinal.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import rospy
 import os.path
 
-import actionlib
+# import actionlib
 import actionlib_msgs.msg as act_msgs # import *
 import geometry_msgs.msg as geo_msgs # geo_msgs.Pose, geo_msgs.PoseWithCovarianceStamped, geo_msgs.Point, geo_msgs.Quaternion, geo_msgs.Twist, geo_msgs.Vector3
 import move_base_msgs.msg as mov_msgs # import mov_msgs.MoveBaseAction, mov_msgs.MoveBaseGoal
